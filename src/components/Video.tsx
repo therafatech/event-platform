157c4eeaee862a5c09d4470e211423f4998c1ba7
Rafael Santos <santosrafael@live.com>
2022-06-24 21:30:44 -0300

    Aula 5

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,5 +1,5 @@
-import { DefaultUi, Player, Vimeo, Youtube } from "@vime/react";
-import { gql, useQuery } from "@apollo/client";
+import { DefaultUi, Player, Youtube } from "@vime/react";
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 import {
   CaretRight,
   CircleNotch,
@@ -11,44 +11,16 @@ import {
 
 import "@vime/core/themes/default.css";
 
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: { slug: $slug }) {
-      title
-      videoId
-      description
-      teacher {
-        bio
-        avatarURL
-        name
-      }
-    }
-  }
-`;
-
-interface GetlessonBySlugResponse {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    };
-  };
-}
-
 interface VideoProps {
   lessonSlug: string;
 }
 
 export function Video(props: VideoProps) {
-  const { data } = useQuery<GetlessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: { slug: props.lessonSlug },
   });
 
-  if (!data)
+  if (!data || !data.lesson)
     return (
       <div className="flex-1 flex justify-center items-center">
         <CircleNotch size={96} className="animate-spin" />
@@ -74,21 +46,23 @@ export function Video(props: VideoProps) {
               {data.lesson.description}
             </p>
 
-            <div className="flex items-center gap-4 mt-6 ">
-              <img
-                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src={data.lesson.teacher.avatarURL}
-                alt="avatar"
-              />
-              <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">
-                  {data.lesson.teacher.name}
-                </strong>
-                <span className="text-gray-200 text-sm block">
-                  {data.lesson.teacher.bio}
-                </span>
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6 ">
+                <img
+                  className="h-16 w-16 rounded-full border-2 border-blue-500"
+                  src={data.lesson.teacher.avatarURL}
+                  alt="avatar"
+                />
+                <div className="leading-relaxed">
+                  <strong className="font-bold text-2xl block">
+                    {data.lesson.teacher.name}
+                  </strong>
+                  <span className="text-gray-200 text-sm block">
+                    {data.lesson.teacher.bio}
+                  </span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
           <div className="flex flex-col gap-4">
             <a
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,4 +1,4 @@
-import { gql, useMutation } from "@apollo/client";
+import { useCreateSubscriberMutation } from "../graphql/generated";
 import { useCallback, useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
@@ -10,21 +10,11 @@ interface InputEvent {
   };
 }
 
-const CREATE_SUBSCRIBER_MUTATION = gql`
-  mutation CreateSubscriber($name: String!, $email: String!) {
-    createSubscriber(data: { name: $name, email: $email }) {
-      id
-    }
-  }
-`;
-
 export function Subscribe() {
   const navigate = useNavigate();
 
   const [form, setForm] = useState({ name: "", email: "" });
-  const [createSubscriber, { loading }] = useMutation(
-    CREATE_SUBSCRIBER_MUTATION
-  );
+  const [createSubscriber, { loading }] = useCreateSubscriberMutation();
 
   const handleChange = useCallback(
     (event: InputEvent) =>
